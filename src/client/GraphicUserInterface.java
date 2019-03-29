93ae07f8d388535ca0ff7147a560781da339d0a4
Isak Eklund <isak.eklund@gmaol.com>
2019-03-29 11:53:15 +0100

    Update GraphicUserInterface.java

--- src/client/GraphicUserInterface.java
+++ src/client/GraphicUserInterface.java
@@ -21,11 +21,11 @@ public class GraphicUserInterface extends JPanel {
 	private JButton btnRandomTable = new JButton("Slumpa bord"); //Send user to random table 
 	private JButton btnConfirmTable = new JButton("Skapa bord"); //After making settings for creating a table 
 	private JButton btnMenu = new JButton("Meny"); //back to menu after a game 
-	
+
 	private JRadioButton radioBtnTime = new JRadioButton();
 	private JRadioButton radioBtnRounds = new JRadioButton();
 	private JRadioButton radioBtnPrivate = new JRadioButton();
-	
+
 	private String[] arrayTime = { "5","10","15","20" };
 	private String[] arrayRounds = { "5","10","15","20" };
 	private String[] arrayBalance = { "100","200","300","400","500" };
@@ -34,14 +34,15 @@ public class GraphicUserInterface extends JPanel {
 	private JComboBox comboBoxRounds = new JComboBox(arrayRounds);
 	private JComboBox comboBoxBalance = new JComboBox(arrayBalance);
 	private JComboBox comboBoxMinBet = new JComboBox(arrayMinBet);
-	
-	
+
+
 	//buttons visible in game 
 	private JButton btnGameX2 = new JButton("X2");
 	private JButton btnGameStop = new JButton("Stop");
 	private JButton btnGameHit = new JButton("Kort");
-	private JButton btnCheat = new JButton("Fuska!");
-	
+	private JButton btnGameCheat = new JButton("Fuska!");
+	private JButton btnGameExit = new JButton("Exit game");
+
 	//Textfields 
 	private JTextField tfUsernameCreate = new JTextField();
 	private JTextField tfPasswordCreate = new JTextField();
@@ -50,14 +51,18 @@ public class GraphicUserInterface extends JPanel {
 	private JTextField tfUsernameLogIn = new JTextField();
 	private JTextField tfPasswordLogIn = new JTextField();
 	private JTextField tfRoomCode = new JTextField();
-	
+
+	private JTextArea taExitScreenPlayers = new JTextArea();
+	private JTextArea taExitScreenPlayerRanks = new JTextArea();
+	private JTextArea taExitScreenAchievements = new JTextArea();
+	private JTextArea taExitScreenNewRank = new JTextArea();
 
 	public GraphicUserInterface(UserController controller) {
 		this.controller = controller;
-//		this.controller.setUI(this);
+		//		this.controller.setUI(this);
 		setLayout(new BorderLayout());
-//		addListners();
-		add(createTableScreen(), BorderLayout.CENTER);
+		//		addListners();
+		add(exitScreen(), BorderLayout.CENTER);
 	}
 
 	public void updateUI(JPanel pane) {
@@ -91,7 +96,7 @@ public class GraphicUserInterface extends JPanel {
 
 		return pane;
 	}
-	
+
 	public JPanel createUserScreen() {
 		JLabel lblUsername = new JLabel("Användarnamn");
 		JLabel lblPassword = new JLabel("Lösenord");
@@ -99,57 +104,52 @@ public class GraphicUserInterface extends JPanel {
 		JLabel lblEmail = new JLabel("Email");
 		GridBagConstraints cont = new GridBagConstraints();
 		JPanel pane = new JPanel(new GridBagLayout());
-//		username.setPreferredSize(new Dimension(200,20));
-//		password.setPreferredSize(new Dimension(200,20));
-//		repPassword.setPreferredSize(new Dimension(200,20));
+		//		username.setPreferredSize(new Dimension(200,20));
+		//		password.setPreferredSize(new Dimension(200,20));
+		//		repPassword.setPreferredSize(new Dimension(200,20));
 		tfUsernameCreate.setPreferredSize(new Dimension(200,20));
 		tfPasswordCreate.setPreferredSize(new Dimension(200,20));
 		tfEmailCreate.setPreferredSize(new Dimension(200,20));
 		tfRepeatPasswordCreate.setPreferredSize(new Dimension(200,20));
-		
+
 		cont.anchor = GridBagConstraints.CENTER;
 		cont.insets = new Insets(10,10,10,10);
-		
+
 		cont.gridx = 0;
 		cont.gridy = 0;
 		pane.add(lblUsername, cont);
-		
+
 		cont.gridx = 1;
-		cont.gridy = 0;
-//		cont.gridwidth = 3;
 		pane.add(tfUsernameCreate);
-		
+
 		cont.gridx = 0;
 		cont.gridy = 1;
 		pane.add(lblEmail, cont);
-		
+
 		cont.gridx = 1;
-		cont.gridy = 1;
 		pane.add(tfEmailCreate, cont);
-		
+
 		cont.gridx = 0;
 		cont.gridy = 2;
 		pane.add(lblPassword, cont);
-		
+
 		cont.gridx = 1;
-		cont.gridy = 2;
 		pane.add(tfPasswordCreate, cont);
-		
+
 		cont.gridx = 0;
 		cont.gridy = 3;
 		pane.add(lblRepeatPassword, cont);
-		
-		cont.gridx = 1;
-		cont.gridy = 3; 
+
+		cont.gridx = 1; 
 		pane.add(tfRepeatPasswordCreate, cont);
-		
+
 		cont.gridx = 1;
 		cont.gridy = 4;
 		pane.add(btnConfirmUser, cont);
-		
+
 		return pane;
 	}
-	
+
 	public JPanel logInScreen() {
 		JLabel lblUsername = new JLabel("Användarnamn");
 		JLabel lblPassword = new JLabel("Lösenord");
@@ -157,33 +157,31 @@ public class GraphicUserInterface extends JPanel {
 		JPanel pane = new JPanel(new GridBagLayout());
 		tfUsernameLogIn.setPreferredSize(new Dimension(200,20));
 		tfPasswordLogIn.setPreferredSize(new Dimension(200,20));
-		
+
 		cont.anchor = GridBagConstraints.CENTER;
 		cont.insets = new Insets(10,10,10,10);
-		
+
 		cont.gridx = 0;
 		cont.gridy = 0;
 		pane.add(lblUsername, cont);
-		
+
 		cont.gridx = 1;
-		cont.gridy = 0;
 		pane.add(tfUsernameLogIn,cont);
-		
+
 		cont.gridx = 0;
 		cont.gridy = 1;
 		pane.add(lblPassword,cont);
-		
+
 		cont.gridx = 1;
-		cont.gridy = 1;
 		pane.add(tfPasswordLogIn,cont);
-		
+
 		cont.gridx = 1;
 		cont.gridy = 2;
 		pane.add(btnConfirmLogIn,cont);
-		
+
 		return pane;
 	}
-	
+
 	public JPanel mainMenuScreen() {
 		JLabel lblPlay = new JLabel("Spela");
 		JLabel lblProfile = new JLabel("Profil");
@@ -191,64 +189,60 @@ public class GraphicUserInterface extends JPanel {
 		JPanel pane = new JPanel(new GridBagLayout());
 		cont.anchor = GridBagConstraints.CENTER;
 		cont.insets = new Insets(10,10,10,10);
-		
+
 		cont.gridx = 0;
 		cont.gridy = 0;
 		pane.add(lblPlay,cont);
-		
+
 		cont.gridx = 1;
-		cont.gridy = 0;
 		pane.add(lblProfile,cont);
-		
+
 		cont.gridx = 0;
 		cont.gridy = 1;
 		pane.add(btnGoToTable,cont);
-		
+
 		cont.gridx = 1;
-		cont.gridy = 1;
 		pane.add(btnAchievements,cont);
-		
+
 		cont.gridx = 0;
 		cont.gridy = 2;
 		pane.add(btnCreateTable,cont);
-		
+
 		cont.gridx = 1;
-		cont.gridy = 2;
 		pane.add(btnRank,cont);
-		
+
 		cont.gridx = 0;
 		cont.gridy = 3;
 		pane.add(btnQuit,cont);
-		
+
 		return pane;
 	}
-	
+
 	public JPanel joinScreen() {
 		JLabel lblRoomCode = new JLabel("Room Code");		
 		GridBagConstraints cont = new GridBagConstraints();
 		JPanel pane = new JPanel(new GridBagLayout());
 		tfRoomCode.setPreferredSize(new Dimension(200,20));
-		
+
 		cont.anchor = GridBagConstraints.CENTER;
 		cont.insets = new Insets(10,10,10,10);
-		
+
 		cont.gridx = 0;
 		cont.gridy = 0;
 		pane.add(lblRoomCode, cont);
-		
+
 		cont.gridy = 1;
 		pane.add(tfRoomCode, cont);
-		
+
 		cont.gridy = 2;
 		pane.add(btnEnterTable, cont);
-		
+
 		cont.gridy = 3;
 		pane.add(btnRandomTable, cont);
-		
-		
+
 		return pane;
 	}
-	
+
 	public JPanel createTableScreen() {
 		JLabel lblTime = new JLabel("Time");
 		JLabel lblRounds = new JLabel("Rounds");
@@ -261,66 +255,165 @@ public class GraphicUserInterface extends JPanel {
 		comboBoxRounds.setPreferredSize(new Dimension(60,20));
 		comboBoxBalance.setPreferredSize(new Dimension(60,20));
 		comboBoxMinBet.setPreferredSize(new Dimension(60,20));
-		
+
 		cont.anchor = GridBagConstraints.CENTER;
 		cont.insets = new Insets(10,10,10,10);
-		
+
 		cont.gridx = 0;
 		cont.gridy = 0;
 		pane.add(radioBtnTime, cont);
-		
+
 		cont.gridx = 1;
 		pane.add(lblTime, cont);
-		
+
 		cont.gridx = 2;
 		pane.add(comboBoxTime, cont);
-		
+
 		cont.gridx = 3;
 		pane.add(lblPrivateMatch, cont);
-		
+
 		cont.gridx = 4;
 		pane.add(radioBtnPrivate, cont);
-		
+
 		cont.gridx = 0;
 		cont.gridy = 1;
 		pane.add(radioBtnRounds, cont);
-		
+
 		cont.gridx = 1;
 		pane.add(lblRounds, cont);
-		
+
 		cont.gridx = 2;
 		pane.add(comboBoxRounds, cont);
-		
+
 		cont.gridx = 1;
 		cont.gridy = 2;
 		pane.add(lblBalance, cont);
-		
+
 		cont.gridx = 2;
 		pane.add(comboBoxBalance, cont);
-		
+
 		cont.gridx = 1;
 		cont.gridy = 3;
 		pane.add(lblMinBet, cont);
-		
+
 		cont.gridx = 2;
 		pane.add(comboBoxMinBet, cont);
-		
+
 		cont.gridx = 4;
 		pane.add(btnConfirmTable, cont);
-		
-		
-		
+
 		return pane;
 	}
-	
+
+	public JPanel gameScreen() {
+		JLabel lblGameScreen = new JLabel(new ImageIcon(new ImageIcon("images/bustedjack.jpg").getImage().getScaledInstance(800, 500, Image.SCALE_DEFAULT)));//Not code to be used later. This is just to get an idea of the game size
+
+		GridBagConstraints cont = new GridBagConstraints();
+		JPanel pane = new JPanel(new GridBagLayout());
+		cont.anchor = GridBagConstraints.CENTER;
+		cont.insets = new Insets(10,10,10,10);
+
+		cont.gridx = 0;
+		cont.gridy = 0;
+		pane.add(btnGameX2, cont);
+
+		cont.gridy = 1;
+		pane.add(btnGameStop, cont);
+
+		cont.gridy = 2;
+		pane.add(btnGameHit, cont);
+
+		cont.gridy = 3;
+		pane.add(btnGameCheat, cont);
+
+		cont.gridx = 1;
+		cont.gridy = 4;
+		pane.add(btnGameExit, cont);
+
+		cont.gridy = 0;
+		cont.gridheight = 4;
+		pane.add(lblGameScreen, cont);
+
+		return pane;
+	}
+
+	public JPanel exitScreen() {
+		JLabel lblPlayers = new JLabel("Players");
+		JLabel lblPlayerRanks = new JLabel("Rank");
+		JLabel lblAchievements = new JLabel("Achievements");
+		JLabel lblNewRank = new JLabel("Rank");
+
+		taExitScreenPlayers.setPreferredSize(new Dimension(160, 470));
+		taExitScreenPlayerRanks.setPreferredSize(new Dimension(160, 470));
+		taExitScreenAchievements.setPreferredSize(new Dimension(180, 160));
+		taExitScreenNewRank.setPreferredSize(new Dimension(180, 160));
+
+		taExitScreenPlayers.setEditable(false);
+		taExitScreenPlayerRanks.setEditable(false);
+		taExitScreenAchievements.setEditable(false);
+		taExitScreenNewRank.setEditable(false);
+
+		GridBagConstraints cont = new GridBagConstraints();
+		JPanel pane = new JPanel(new GridBagLayout());
+		cont.anchor = GridBagConstraints.CENTER;
+		cont.insets = new Insets(10,10,10,10);
+
+		cont.gridx = 0;
+		cont.gridy = 0;
+		pane.add(lblPlayers, cont);
+
+		cont.gridx = 1;
+		pane.add(lblPlayerRanks, cont);
+
+		cont.gridx = 2;
+		pane.add(lblAchievements, cont);
+
+		cont.gridx = 0;
+		cont.gridy = 1;
+		cont.gridheight = 4;
+		pane.add(taExitScreenPlayers, cont);
+
+		cont.gridx = 1;
+		cont.gridheight = 4;
+		pane.add(taExitScreenPlayerRanks, cont);
+
+		cont.gridx = 2;
+		cont.gridheight = 1;
+		pane.add(taExitScreenAchievements, cont);
+
+		cont.gridy = 2;
+		pane.add(lblNewRank, cont);
+
+		cont.gridy = 3;
+		pane.add(taExitScreenNewRank, cont);
+
+		cont.gridy = 4;
+		pane.add(btnMenu, cont);
+
+		return pane;
+	}
+
 	public void addListener() {
 		ActionL listner = new ActionL();
+		btnLogIn.addActionListener(listner);
+		btnCreateUser.addActionListener(listner);
+		btnConfirmUser.addActionListener(listner);
+		btnConfirmLogIn.addActionListener(listner);
+		btnGoToTable.addActionListener(listner);
+		btnCreateTable.addActionListener(listner);
+		btnQuit.addActionListener(listner);
+		btnAchievements.addActionListener(listner);
+		btnRank.addActionListener(listner);
+		btnEnterTable.addActionListener(listner);
+		btnRandomTable.addActionListener(listner);
+		btnConfirmTable.addActionListener(listner);
+		btnMenu.addActionListener(listner);
 	}
-	
+
 	private class ActionL implements ActionListener{
-		
+
 		public void actionPerformed(ActionEvent e) {
-			
+
 		}
 	}
 
@@ -328,7 +421,7 @@ public class GraphicUserInterface extends JPanel {
 		UserController controller = new UserController();
 		GraphicUserInterface gui = new GraphicUserInterface(controller);
 		JFrame frame = new JFrame();
-		frame.setPreferredSize(new Dimension(500,300));
+		frame.setPreferredSize(new Dimension(1000,600));
 		frame.setResizable(false);
 		frame.setVisible(true);
 		frame.pack();
