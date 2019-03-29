af26c19116d33db70950c6e37b3d3535f06007ba
Chofen <chof@palvin.se>
2019-03-29 10:48:06 +0100

    Update GraphicUserInterface.java
    JoinScreen and createTableScreen

--- src/client/GraphicUserInterface.java
+++ src/client/GraphicUserInterface.java
@@ -16,12 +16,26 @@ public class GraphicUserInterface extends JPanel {
 	private JButton btnCreateTable = new JButton("Skapa bord"); //From main menu
 	private JButton btnQuit = new JButton("Avsluta"); //Sends form main menu back to log in screen 
 	private JButton btnAchievements = new JButton("Achievements"); //From main menu
-	private JButton btnRank = new JButton("Rank"); //From main menu, shows curretn rank
+	private JButton btnRank = new JButton("Rank"); //From main menu, shows current rank
 	private JButton btnEnterTable = new JButton("Gå till bord"); //Confirm after entering table code 
 	private JButton btnRandomTable = new JButton("Slumpa bord"); //Send user to random table 
 	private JButton btnConfirmTable = new JButton("Skapa bord"); //After making settings for creating a table 
 	private JButton btnMenu = new JButton("Meny"); //back to menu after a game 
-
+	
+	private JRadioButton radioBtnTime = new JRadioButton();
+	private JRadioButton radioBtnRounds = new JRadioButton();
+	private JRadioButton radioBtnPrivate = new JRadioButton();
+	
+	private String[] arrayTime = { "5","10","15","20" };
+	private String[] arrayRounds = { "5","10","15","20" };
+	private String[] arrayBalance = { "100","200","300","400","500" };
+	private String[] arrayMinBet = { "10","50","100","150" };
+	private JComboBox comboBoxTime = new JComboBox(arrayTime);
+	private JComboBox comboBoxRounds = new JComboBox(arrayRounds);
+	private JComboBox comboBoxBalance = new JComboBox(arrayBalance);
+	private JComboBox comboBoxMinBet = new JComboBox(arrayMinBet);
+	
+	
 	//buttons visible in game 
 	private JButton btnGameX2 = new JButton("X2");
 	private JButton btnGameStop = new JButton("Stop");
@@ -35,13 +49,15 @@ public class GraphicUserInterface extends JPanel {
 	private JTextField tfEmailCreate = new JTextField();
 	private JTextField tfUsernameLogIn = new JTextField();
 	private JTextField tfPasswordLogIn = new JTextField();
+	private JTextField tfRoomCode = new JTextField();
+	
 
 	public GraphicUserInterface(UserController controller) {
 		this.controller = controller;
 //		this.controller.setUI(this);
 		setLayout(new BorderLayout());
 //		addListners();
-		add(mainMenuScreen(), BorderLayout.CENTER);
+		add(createTableScreen(), BorderLayout.CENTER);
 	}
 
 	public void updateUI(JPanel pane) {
@@ -203,6 +219,96 @@ public class GraphicUserInterface extends JPanel {
 		cont.gridx = 0;
 		cont.gridy = 3;
 		pane.add(btnQuit,cont);
+		
+		return pane;
+	}
+	
+	public JPanel joinScreen() {
+		JLabel lblRoomCode = new JLabel("Room Code");		
+		GridBagConstraints cont = new GridBagConstraints();
+		JPanel pane = new JPanel(new GridBagLayout());
+		tfRoomCode.setPreferredSize(new Dimension(200,20));
+		
+		cont.anchor = GridBagConstraints.CENTER;
+		cont.insets = new Insets(10,10,10,10);
+		
+		cont.gridx = 0;
+		cont.gridy = 0;
+		pane.add(lblRoomCode, cont);
+		
+		cont.gridy = 1;
+		pane.add(tfRoomCode, cont);
+		
+		cont.gridy = 2;
+		pane.add(btnEnterTable, cont);
+		
+		cont.gridy = 3;
+		pane.add(btnRandomTable, cont);
+		
+		
+		return pane;
+	}
+	
+	public JPanel createTableScreen() {
+		JLabel lblTime = new JLabel("Time");
+		JLabel lblRounds = new JLabel("Rounds");
+		JLabel lblBalance = new JLabel("Balance");
+		JLabel lblMinBet = new JLabel("Minimum Bet");
+		JLabel lblPrivateMatch = new JLabel("Privte Match");
+		GridBagConstraints cont = new GridBagConstraints();
+		JPanel pane = new JPanel(new GridBagLayout());
+		comboBoxTime.setPreferredSize(new Dimension(60,20));
+		comboBoxRounds.setPreferredSize(new Dimension(60,20));
+		comboBoxBalance.setPreferredSize(new Dimension(60,20));
+		comboBoxMinBet.setPreferredSize(new Dimension(60,20));
+		
+		cont.anchor = GridBagConstraints.CENTER;
+		cont.insets = new Insets(10,10,10,10);
+		
+		cont.gridx = 0;
+		cont.gridy = 0;
+		pane.add(radioBtnTime, cont);
+		
+		cont.gridx = 1;
+		pane.add(lblTime, cont);
+		
+		cont.gridx = 2;
+		pane.add(comboBoxTime, cont);
+		
+		cont.gridx = 3;
+		pane.add(lblPrivateMatch, cont);
+		
+		cont.gridx = 4;
+		pane.add(radioBtnPrivate, cont);
+		
+		cont.gridx = 0;
+		cont.gridy = 1;
+		pane.add(radioBtnRounds, cont);
+		
+		cont.gridx = 1;
+		pane.add(lblRounds, cont);
+		
+		cont.gridx = 2;
+		pane.add(comboBoxRounds, cont);
+		
+		cont.gridx = 1;
+		cont.gridy = 2;
+		pane.add(lblBalance, cont);
+		
+		cont.gridx = 2;
+		pane.add(comboBoxBalance, cont);
+		
+		cont.gridx = 1;
+		cont.gridy = 3;
+		pane.add(lblMinBet, cont);
+		
+		cont.gridx = 2;
+		pane.add(comboBoxMinBet, cont);
+		
+		cont.gridx = 4;
+		pane.add(btnConfirmTable, cont);
+		
+		
 		
 		return pane;
 	}
