d86596815a57dcd73eb50b99d881a5076db954b7
Isak Eklund <isak.eklund@gmaol.com>
2019-04-03 11:10:59 +0200

    Created a StartClient class
    Moved main from UI to a new class, StartClient

---
+++ src/client/StartClient.java
@@ -0,0 +1,21 @@
+package client;
+
+import java.awt.Dimension;
+import java.io.IOException;
+
+import javax.swing.JFrame;
+
+public class StartClient {
+	public static void main(String[] args) throws IOException {
+		UserClient client = new UserClient("10.2.5.130", 1200);
+		UserController controller = new UserController(client);
+		UserInterface gui = new UserInterface(controller);
+		JFrame frame = new JFrame();
+		frame.setPreferredSize(new Dimension(1000,600));
+		frame.setResizable(false);
+		frame.setVisible(true);
+		frame.pack();
+		frame.add(gui);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
