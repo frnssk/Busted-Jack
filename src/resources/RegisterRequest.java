a7c4844835e7b8aeb493b4b0c0f27f644cd6e0e0
Rasmus Oberg <rasmus.oberg@hotmail.com>
2019-04-10 10:51:00 +0200

    Update RegisterRequest.java

--- src/resources/RegisterRequest.java
+++ src/resources/RegisterRequest.java
@@ -2,11 +2,11 @@ package resources;
 
 import java.io.Serializable;
 
-public class RegisterRequest implements Serializable {
+public class RegisterRequest {
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = 2001217030614121593L;
+//	private static final long serialVersionUID = 2001217030614121593L;
 	private String username;
 	private char[] password;
 	
